c72545a2e81101add3dbee60d625ae837b864c97
kseme277 <kseme277@gmail.com>
2025-11-11 20:07:18 +0100

    Implémentation de la fonction factoriel

--- src/main/java/com/stjean/operation/OperationMathematique.java
+++ src/main/java/com/stjean/operation/OperationMathematique.java
@@ -12,4 +12,16 @@ public class OperationMathematique {
     public boolean estPositif(int nombre) {
         return nombre > 0;
     }
+    
+  
+    public long factoriel(int nombre) {
+        if (nombre == 0 || nombre == 1) {
+            return 1;
+        }
+        long resultat = 1;
+        for (int i = 2; i <= nombre; i++) {
+            resultat *= i;
+        }
+        return resultat;
+    }
 }
