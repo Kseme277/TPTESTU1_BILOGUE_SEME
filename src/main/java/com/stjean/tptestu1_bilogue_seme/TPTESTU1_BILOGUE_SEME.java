4e8d35b857a1c638b289d3c4ef206922cd44df47
kseme277 <kseme277@gmail.com>
2025-11-11 19:31:05 +0100

    Projet initial Maven

---
+++ src/main/java/com/stjean/tptestu1_bilogue_seme/TPTESTU1_BILOGUE_SEME.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.stjean.tptestu1_bilogue_seme;
+
+/**
+ *
+ * @author kseme
+ */
+public class TPTESTU1_BILOGUE_SEME {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
