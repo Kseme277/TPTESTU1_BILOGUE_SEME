2728754d62f53bc2e3ac45e74bacfbff6d55df68
kseme277 <kseme277@gmail.com>
2025-11-11 20:25:58 +0100

    Implémentation d'une exception

---
+++ src/main/java/com/stjean/operation/IllegalParamException.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.stjean.operation;
+
+/**
+ *
+ * @author kseme
+ */
+
+
+public class IllegalParamException extends Exception {
+    public IllegalParamException(String message) {
+        super(message);
+    }
+}
